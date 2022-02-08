4548a86587b6a56f3588e24df901e8fa122f0af6
Sudesh Nishshanka Bandara <33403844+sudeshnb@users.noreply.github.com>
2022-02-08 16:38:35 +0530

    Update README.md

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -67,39 +67,42 @@ class _HomePageState extends State<HomePage> {
                   const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
               child: Row(
                 children: [
-                  Container(
-                    height: 45.0,
-                    width: 300.0,
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    decoration: BoxDecoration(
-                      color: white,
-                      border: Border.all(color: green),
-                      boxShadow: [
-                        BoxShadow(
-                          color: green.withOpacity(0.15),
-                          blurRadius: 10,
-                          offset: const Offset(0, 0),
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Row(
-                      children: [
-                        const SizedBox(
-                          height: 45,
-                          width: 250,
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: 'Search',
+                  Expanded(
+                    child: Container(
+                      height: 45.0,
+                      width: 300.0,
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      decoration: BoxDecoration(
+                        color: white,
+                        border: Border.all(color: green),
+                        boxShadow: [
+                          BoxShadow(
+                            color: green.withOpacity(0.15),
+                            blurRadius: 10,
+                            offset: const Offset(0, 0),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                           const SizedBox(
+                            height: 45,
+                            width: 148,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Search',
+                              ),
                             ),
                           ),
-                        ),
-                        Image.asset(
-                          'assets/icons/search.png',
-                          height: 25,
-                        )
-                      ],
+                          Image.asset(
+                            'assets/icons/search.png',
+                            height: 25,
+                          )
+                        ],
+                      ),
                     ),
                   ),
                   const SizedBox(width: 10),
